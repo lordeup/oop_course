a5f6322b28227a276246aab4f18f52350ee58714
Andrey Kalanov <lordeup@gmail.com>
2019-02-14 15:34:51 +0300

    Отредактировал программу findtext.exe

--- lw1/findtext/findtext/findtext.cpp
+++ lw1/findtext/findtext/findtext.cpp
@@ -7,15 +7,15 @@ bool FindTextInFile(std::ifstream& fileInput, std::string& searchString)
 {
 	bool found = false;
 	std::string line;
-	int lineNumber = 1;
+	int lineNumber{};
 	while (getline(fileInput, line))
 	{
+		++lineNumber;
 		if (line.find(searchString) != std::string::npos)
 		{
 			found = true;
 			std::cout << "Line number: " << lineNumber << std::endl;
 		}
-		++lineNumber;
 	}
 
 	return found;
@@ -23,18 +23,20 @@ bool FindTextInFile(std::ifstream& fileInput, std::string& searchString)
 
 int main(int argc, char* argv[])
 {
-	if (argc != 3)
+	if (argc < 3)
 	{
-		std::cerr << "The number of arguments does not match the task condition." << std::endl;
+		std::cerr << "The number of arguments does not match the task condition" << std::endl
+				  << "The input should look like <file name> <text to search>" << std::endl;
 		return 1;
 	}
 
-	std::ifstream fileInput(argv[1]);
+	std::string fileName = argv[1];
+	std::ifstream fileInput(fileName);
 	std::string searchString = argv[2];
 
 	if (!fileInput.is_open())
 	{
-		std::cerr << "The file is not open" << std::endl;
+		std::cerr << "This file does not exist" << std::endl;
 		return 1;
 	}
 
--- lw1/findtext/findtext/test.bat
+++ lw1/findtext/findtext/test.bat
@@ -1,56 +1,50 @@
 @echo off
 
+chcp 1251
+
 set PROGRAM="%~1"
 
 set OUT="%TEMP%\out.txt"
 
-echo No params
-call %PROGRAM%  2>%OUT%
+call %PROGRAM% 2> %OUT%
 fc %OUT% tests\noParams-out.txt
 if errorlevel 1 goto err
-echo Test: No params SUCCEED
+echo Test: No params PASSED
 
-echo File missing
 call %PROGRAM% tests\missing.txt "Lorem ipsum" > %OUT%
 fc %OUT% tests\fileMissing-out.txt
 if not errorlevel 0 goto err
-echo Test: File missing SUCCEED
+echo Test: File missing PASSED
 
-echo Empty search string
 call %PROGRAM% tests\textFile.txt "" > %OUT%
 fc %OUT% tests\emptyString-out.txt
 if not errorlevel 0 goto err
-echo Test: Empty search string SUCCEED
+echo Test: Empty search string PASSED
 
-echo Empty text file
 call %PROGRAM% tests\textFileEmpty.txt "Lorem" > %OUT%
 fc %OUT% tests\textFileEmpty-out.txt
 if not errorlevel 1 goto err
-echo Test: Empty text file SUCCEED
+echo Test: Empty text file PASSED
 
-echo Text not found
 call %PROGRAM% tests\textFile.txt "Elon Musk" > %OUT%
 fc %OUT% tests\textNotFound-out.txt
 if not errorlevel 0 goto err
-echo Test: Text not found SUCCEED
+echo Test: Text not found PASSED
 
-echo Find the text in file
 call %PROGRAM% tests\textFile.txt "Lorem ipsum" > %OUT%
 fc %OUT% tests\findTextFile-out.txt
 if errorlevel 1 goto err
-echo Test: Find the text in file SUCCEED
+echo Test: Find the text in file PASSED
 
-echo Search expressions are case-sensitive
 call %PROGRAM% tests\textFile.txt "Amet" > %OUT%
 fc %OUT% tests\textRegister-out.txt
 if errorlevel 1 goto err
-echo Test: Search expressions are case-sensitive SUCCEED
+echo Test: Search expressions are case-sensitive PASSED
 
-echo Searching repeated
 call %PROGRAM% tests\textFile.txt "sir" > %OUT%
 fc %OUT% tests\textRepeated-out.txt
 if errorlevel 1 goto err
-echo Test: Searching repeated SUCCEED
+echo Test: Searching repeated PASSED
 
 echo All tests passed
 exit / B 0
