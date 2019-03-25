8b10c66f83a28cb1d521e590342b9cb87d8e44fd
Andrey Kalanov <lordeup@gmail.com>
2019-03-26 00:20:49 +0300

    Добавлены тесты к заданию GeneratePrimeNumbersSet

---
+++ lw2/4/GeneratePrimeNumbersSetTests/tests.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "../../4/GeneratePrimeNumbersSet/Primes.h"
+
+
+TEST_CASE("InitVectorOfPrimes test")
+{
+	std::vector<bool> outVecOne = { true, true, true };
+	CHECK(InitVectorOfPrimes(2) == outVecOne);
+
+	std::vector<bool> outVecTwo = { true, true, true, true, false, true, false };
+	CHECK(InitVectorOfPrimes(6) == outVecTwo);
+
+	std::vector<bool> outVecThree = { true, true, true, true, false, true, false, true, false, false, false };
+	CHECK(InitVectorOfPrimes(10) == outVecThree);
+}
+
+TEST_CASE("GeneratePrimeNumbersSet test size")
+{
+	CHECK(GeneratePrimeNumbersSet(2).size() == 1);
+	CHECK(GeneratePrimeNumbersSet(20).size() == 8);
+	CHECK(GeneratePrimeNumbersSet(100).size() == 25);
+	CHECK(GeneratePrimeNumbersSet(MAX_SIZE).size() == 5761455);
+}
+
+TEST_CASE("GeneratePrimeNumbersSet test element")
+{
+	std::set<int> outSetOne = { 2 };
+	CHECK(GeneratePrimeNumbersSet(2) == outSetOne);
+
+	std::set<int> outSetTwo = { 2, 3, 5, 7, 11, 13, 17, 19 };
+	CHECK(GeneratePrimeNumbersSet(20) == outSetTwo);
+
+	std::set<int> outSetThree = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29,31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+	CHECK(GeneratePrimeNumbersSet(100) == outSetThree);
+}
