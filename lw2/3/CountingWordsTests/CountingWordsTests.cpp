eca80c9a30bc99b597efde33531320eda1e568b0
Andrey Kalanov <lordeup@gmail.com>
2019-03-30 23:48:23 +0300

    Добавлены тесты к заданию CountingWords

---
+++ lw2/3/CountingWordsTests/CountingWordsTests.cpp
@@ -0,0 +1,38 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "../../3/CountingWords/Process.h"
+#include <catch2/catch.hpp>
+
+TEST_CASE("CountWord test empty string")
+{
+	std::string str = "";
+	WordFrequency outMap = {};
+	CHECK(CountWord(str) == outMap);
+}
+
+TEST_CASE("CountWord test string with space")
+{
+	std::string str = "       ";
+	WordFrequency outMap = {};
+	CHECK(CountWord(str) == outMap);
+}
+
+TEST_CASE("CountWord test one words")
+{
+	std::string str = "HEllo";
+	WordFrequency outMap = { { "hello", 1 } };
+	CHECK(CountWord(str) == outMap);
+}
+
+TEST_CASE("CountWord test repeat words")
+{
+	std::string str = "WORLD world WORLd";
+	WordFrequency outMap = { { "world", 3 } };
+	CHECK(CountWord(str) == outMap);
+}
+
+TEST_CASE("CountWord test long string")
+{
+	std::string str = "But I must EXPLAIN of AND denouncing pleasure and";
+	WordFrequency outMap = { { "and", 2 }, { "but", 1 }, { "denouncing", 1 }, { "explain", 1 }, { "i", 1 }, { "must", 1 }, { "of", 1 }, { "pleasure", 1 } };
+	CHECK(CountWord(str) == outMap);
+}
